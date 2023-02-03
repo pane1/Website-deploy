f79d4c6afceb485699ff2be6b8cca8295bb9f8aa
pane1 <ericpan1998@hotmail.com>
2023-02-02 22:52:13 -0500

    removed aws

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "stockwebsite": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
